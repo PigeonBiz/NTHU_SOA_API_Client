5507ad88a8fd768d56370deed09b533e6dc1a086
KaiyiChu <49907547+s28238385@users.noreply.github.com>
2022-10-21 16:08:41 +0800

    Revert "Api youtube explore"

--- lib/project_info.rb
+++ lib/project_info.rb
@@ -4,34 +4,32 @@ require 'http'
 require 'yaml'
 
 config = YAML.safe_load(File.read('../config/secrets.yml'))
-SEARCH_KEY_WORD = 'taylor%20swift%20offical'
-SEARCH_RESULT_NUM = 5
 
-def yt_api_path(search, config, result_num)
-  "https://www.googleapis.com/youtube/v3/search?part=snippet&q=#{search}&key=#{config['ACCESS_TOKEN']}&type=video&maxResults=#{result_num}"
+def fb_api_path(path, config)
+  "https://graph.facebook.com/v15.0/#{path}&access_token=#{config['ACCESS_TOKEN']}"
 end
 
-def call_yt_url(url)
+def call_fb_url(url)
   HTTP.headers('Accept' => 'application/json').get(url)
 end
 
-yt_response = {}
-yt_results = {}
+fb_response = {}
+fb_results = {}
 
-## HAPPY search request
-search_url = yt_api_path(SEARCH_KEY_WORD, config, SEARCH_RESULT_NUM)
-yt_response[search_url] = call_yt_url(search_url)
-search_result = yt_response[search_url].parse
+## HAPPY project request
+project_url = fb_api_path('me?fields=name%2Cbirthday%2Cemail%2Cfriends', config)
+fb_response[project_url] = call_fb_url(project_url)
+project = fb_response[project_url].parse
 
-yt_results['kind'] = search_result['kind']
-yt_results['etag'] = search_result['etag']
-yt_results['nextPageToken'] = search_result['nextPageToken']
-yt_results['regionCode'] = search_result['regionCode']
-yt_results['items'] = search_result['items']
+fb_results['name'] = project['name']
+fb_results['birthday'] = project['birthday']
+fb_results['email'] = project['email']
+fb_results['friends_total_count'] = project['friends']['summary']['total_count']
 
-# BAD search request
-bad_search_url = yt_api_path(SEARCH_KEY_WORD, 'bad_token', SEARCH_RESULT_NUM)
-yt_response[bad_search_url] = call_yt_url(bad_search_url)
-yt_response[bad_search_url].parse
+# BAD project request
+bad_project_url = fb_api_path('soumya.ray.prof?fields=id%2Cname%2Cbirthday%2Cemail%2Cfriends', config)
 
-File.write('../spec/fixtures/yt_results.yml', yt_results.to_yaml)
+fb_response[bad_project_url] = call_fb_url(bad_project_url)
+fb_response[bad_project_url].parse
+
+File.write('../spec/fixtures/fb_results.yml', fb_results.to_yaml)
